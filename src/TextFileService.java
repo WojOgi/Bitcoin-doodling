b3202fb043554e30e6568a8ac6c81bc5ed1bc1da
xast89 <xast@wp.pl>
2023-01-16 12:07:05 +0100

    Update TextFileService.java

--- src/TextFileService.java
+++ src/TextFileService.java
@@ -6,7 +6,11 @@ import java.nio.file.Paths;
 
 public class TextFileService {
 
+    // po co to pole 'textToWriteToFile' ?
     private String textToWriteToFile;
+    
+    // to "C:\\Users..." wyrzuciłbym do pola private statif final PATH_TO_COS_TAM
+    // i tak btw - u mnie na linuxie to nie zadziała :( :( :( :(  Bo ja nie mam C:\\.,,,,
     Path path = Paths.get("C:\\Users\\ogieglw\\Dropbox\\! temp stuff\\doodle.txt");
 
     public TextFileService() {
