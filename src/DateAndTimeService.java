4d504c013e031454edc8a2f85be8492eb4775846
WojOgi <w.ogieglo@gmail.com>
2023-01-17 20:41:05 +0300

    first cleanup

--- src/DateAndTimeService.java
+++ src/DateAndTimeService.java
@@ -5,7 +5,7 @@ public class DateAndTimeService {
 
     private static final SimpleDateFormat FORMAT = new SimpleDateFormat("dd/MM/yyyy HH:mm:ss");
     // pole statyczne jest współdzielone przez wszystkie instancje, format jest tylko jeden oraz
-    private int timeIntervalInMiliseconds = 2*60*1000;
+    private int timeIntervalInMiliseconds = 5*60*1000;
 //minuty, private static final i DUZYMI
 
     public int getTimeIntervalInMiliseconds() {
